38ccf6b8e629bed6fa4bc5f256205a9c42d4cfb7
sandokhan <rhaydrick@gmail.com>
2025-01-06 05:36:14 +0000

    Add: Solved String exercises

--- exercicios.py
+++ exercicios.py
@@ -22,31 +22,44 @@
 # #### Números de Ponto Flutuante (`float`)
 
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
-print(f"A soma dos números é: {num1 + num2}")
-# 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
-print(f"A média dos números é: {(num1 + num2) / 2}")
-# 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
-num1 = float(input("Digite a base: "))
-num2 = float(input("Digite o expoente: "))
-print(f"A média dos números é: {num1 ** num2}")
-# 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
-c = float(input("Digite a temperatura em Celsius: "))
-print(f"A temperatura em Fahrenheit é: {(c * 1.8) + 32}")
-# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
-r = float(input("Digite o raio do círculo: "))
-print(f"A área do círculo é: {3.14 * r ** 2}")
+# num1 = float(input("Digite o primeiro número: "))
+# num2 = float(input("Digite o segundo número: "))
+# print(f"A soma dos números é: {num1 + num2}")
+# # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+# num1 = float(input("Digite o primeiro número: "))
+# num2 = float(input("Digite o segundo número: "))
+# print(f"A média dos números é: {(num1 + num2) / 2}")
+# # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+# num1 = float(input("Digite a base: "))
+# num2 = float(input("Digite o expoente: "))
+# print(f"A média dos números é: {num1 ** num2}")
+# # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+# c = float(input("Digite a temperatura em Celsius: "))
+# print(f"A temperatura em Fahrenheit é: {(c * 1.8) + 32}")
+# # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+# r = float(input("Digite o raio do círculo: "))
+# print(f"A área do círculo é: {3.14 * r ** 2}")
 
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+string = input("Digite uma string: ")
+print(f"A string em maiúsculas é: {string.upper()}")
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+nome = input("Digite seu nome completo: ")
+print(f"Seu nome em minúsculas é: {nome.lower()}")
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+frase = input("Digite uma frase: ")
+print(f"A frase sem espaços em branco é: {frase.strip()}")
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+data = input("Digite uma data no formato 'dd/mm/aaaa': ")
+print(f"O dia é: {data.split("/")[0]}")
+print(f"O mês é: {data.split("/")[1]}")
+print(f"O ano é: {data.split("/")[2]}")
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+string1 = input("Digite a primeira string: ")
+string2 = input("Digite a segunda string: ")
+print(f"A concatenação das strings é: {string1 +" "+ string2}")
 
 # #### Booleanos (`bool`)
 
